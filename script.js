c421cd34bcd8df25affe71bf8fa9d2c69c813052
musyokijohn <110732043+musyokijohn@users.noreply.github.com>
2026-01-31 08:58:24 +0300

    added animation transition to some sections

--- script.js
+++ script.js
@@ -12,4 +12,21 @@ closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
 
+// ================================
+// On Scroll Animations
+// ================================
+const animatedElements = document.querySelectorAll(".animate-on-scroll");
+
+const scrollObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+animatedElements.forEach(el => scrollObserver.observe(el));
 
